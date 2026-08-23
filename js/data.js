60f57adae7d1d4e417df7a638b26d2ec06c498dc
RS6048 <121669335+RS6048@users.noreply.github.com>
2026-08-23 10:56:06 +0800

    Add status tags (Ongoing/Halt/Finished) as first tag for each test project

--- js/data.js
+++ js/data.js
@@ -4,56 +4,56 @@ const projectsData = [
         title: 'Test Project',
         description: 'To make sure it works.',
         folder: 'project-detail',
-        tags: ['Hello', 'World'],
+        tags: ['Finished', 'Hello', 'World'],
         detail: 'Find out what is in here!'
     },
     {
         title: 'Test Project Alpha',
         description: 'First batch of test data for scrolling.',
         folder: 'project-detail',
-        tags: ['Test', 'Alpha'],
+        tags: ['Ongoing', 'Test', 'Alpha'],
         detail: 'Alpha test project detail.'
     },
     {
         title: 'Test Project Beta',
         description: 'Second test entry to fill the list.',
         folder: 'project-detail',
-        tags: ['Test', 'Beta'],
+        tags: ['Halt', 'Test', 'Beta'],
         detail: 'Beta test project detail.'
     },
     {
         title: 'Test Project Gamma',
         description: 'Third entry, keep scrolling down.',
         folder: 'project-detail',
-        tags: ['Test', 'Gamma'],
+        tags: ['Finished', 'Test', 'Gamma'],
         detail: 'Gamma test project detail.'
     },
     {
         title: 'Test Project Delta',
         description: 'Fourth entry, almost there.',
         folder: 'project-detail',
-        tags: ['Test', 'Delta'],
+        tags: ['Ongoing', 'Test', 'Delta'],
         detail: 'Delta test project detail.'
     },
     {
         title: 'Test Project Epsilon',
         description: 'Fifth entry, the list is getting long.',
         folder: 'project-detail',
-        tags: ['Test', 'Epsilon'],
+        tags: ['Halt', 'Test', 'Epsilon'],
         detail: 'Epsilon test project detail.'
     },
     {
         title: 'Test Project Zeta',
         description: 'Sixth entry, scrollbar should be visible now.',
         folder: 'project-detail',
-        tags: ['Test', 'Zeta'],
+        tags: ['Finished', 'Test', 'Zeta'],
         detail: 'Zeta test project detail.'
     },
     {
         title: 'Test Project Eta',
         description: 'Seventh and final test entry at the bottom.',
         folder: 'project-detail',
-        tags: ['Test', 'Eta'],
+        tags: ['Ongoing', 'Test', 'Eta'],
         detail: 'Eta test project detail.'
     }
 ];
