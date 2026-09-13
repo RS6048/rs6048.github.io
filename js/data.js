107d59c706bff24b182732c2067b7840ee3eb027
RS6048 <121669335+RS6048@users.noreply.github.com>
2026-09-13 13:45:32 +0800

    Reset project data: remove test projects, add Turbowarp with lastUpdate

--- js/data.js
+++ js/data.js
@@ -1,59 +1,11 @@
 // Data for all projects
 const projectsData = [
     {
-        title: 'Test Project',
-        description: 'To make sure it works.',
-        folder: 'project-detail',
-        tags: ['Finished', 'Hello', 'World'],
-        detail: 'Find out what is in here!'
-    },
-    {
-        title: 'Test Project Alpha',
-        description: 'First batch of test data for scrolling.',
-        folder: 'project-detail',
-        tags: ['Ongoing', 'Test', 'Alpha'],
-        detail: 'Alpha test project detail.'
-    },
-    {
-        title: 'Test Project Beta',
-        description: 'Second test entry to fill the list.',
-        folder: 'project-detail',
-        tags: ['Halt', 'Test', 'Beta'],
-        detail: 'Beta test project detail.'
-    },
-    {
-        title: 'Test Project Gamma',
-        description: 'Third entry, keep scrolling down.',
-        folder: 'project-detail',
-        tags: ['Finished', 'Test', 'Gamma'],
-        detail: 'Gamma test project detail.'
-    },
-    {
-        title: 'Test Project Delta',
-        description: 'Fourth entry, almost there.',
-        folder: 'project-detail',
-        tags: ['Ongoing', 'Test', 'Delta'],
-        detail: 'Delta test project detail.'
-    },
-    {
-        title: 'Test Project Epsilon',
-        description: 'Fifth entry, the list is getting long.',
-        folder: 'project-detail',
-        tags: ['Halt', 'Test', 'Epsilon'],
-        detail: 'Epsilon test project detail.'
-    },
-    {
-        title: 'Test Project Zeta',
-        description: 'Sixth entry, scrollbar should be visible now.',
-        folder: 'project-detail',
-        tags: ['Finished', 'Test', 'Zeta'],
-        detail: 'Zeta test project detail.'
-    },
-    {
-        title: 'Test Project Eta',
-        description: 'Seventh and final test entry at the bottom.',
-        folder: 'project-detail',
-        tags: ['Ongoing', 'Test', 'Eta'],
-        detail: 'Eta test project detail.'
+        title: 'Turbowarp',
+        description: 'A guide page for my Turbowarp projects.',
+        folder: 'Turbowarp',
+        tags: ['Ongoing'],
+        lastUpdate: '2026-09-13',
+        detail: 'This is a guide page recording some of my Turbowarp projects.'
     }
 ];
